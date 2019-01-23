49995fcb43bbfa65ba90c568c6f358eeb8175e48
Mike Cluck <mcluck90@gmail.com>
2019-01-23 09:25:45 -0700

    Added basic profiling options and sped up the live_neighbor_count function

--- www/index.js
+++ www/index.js
@@ -1,6 +1,49 @@
 import { Universe, Cell } from 'wasm-game-of-life';
 import { memory } from 'wasm-game-of-life/wasm_game_of_life_bg';
 
+const fps = new class {
+	constructor() {
+		this.fps = document.getElementById('fps');
+		this.frames = [];
+		this.lastFrameTimeStamp = performance.now();
+	}
+
+	render() {
+		// Convert the delta time since the last frame render into a measure
+		// of frames per second.
+		const now = performance.now();
+		const delta = now - this.lastFrameTimeStamp;
+		this.lastFrameTimeStamp = now;
+		const fps = 1 / delta * 1000;
+
+		// Save only the latest 100 timings.
+		this.frames.push(fps);
+		if (this.frames.length > 100) {
+			this.frames.shift();
+		}
+
+		// Find the max, min, and mean of our 100 latest timings.
+		let min = Infinity;
+		let max = -Infinity;
+		let sum = 0;
+		for (let frame of this.frames) {
+			sum += frame;
+			min = Math.min(min, frame);
+			max = Math.max(max, frame);
+		}
+		let mean = sum / this.frames.length;
+
+		// Render the statistics.
+		this.fps.textContent = `
+Frames per Second:
+				 latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+			`.trim();
+	}
+}
+
 const CELL_SIZE = 5; // px
 const GRID_COLOR = '#CCCCCC';
 const DEAD_COLOR = '#FFFFFF';
@@ -24,6 +67,7 @@ canvas.width = (CELL_SIZE + 1) * width + 1;
 const ctx = canvas.getContext('2d');
 
 const renderLoop = () => {
+	fps.render();
 	for (let i = 0; i < ticksPerFrame; i++) {
 		universe.tick();
 	}
@@ -61,16 +105,38 @@ const drawCells = () => {
 
 	ctx.beginPath();
 
+	// Alive cells
+	ctx.fillStyle = ALIVE_COLOR;
 	for (let row = 0; row < height; row++) {
 		for (let col = 0; col < height; col++) {
 			const idx = getIndex(row, col);
+			if (cells[idx] !== Cell.Alive) {
+				continue;
+			}
 
-			ctx.fillStyle = cells[idx] === Cell.Dead ? DEAD_COLOR : ALIVE_COLOR;
 			ctx.fillRect(
 				col * (CELL_SIZE + 1) + 1,
 				row * (CELL_SIZE + 1) + 1,
 				CELL_SIZE,
-				CELL_SIZE
+				CELL_SIZE,
+			);
+		}
+	}
+
+	// Dead cells
+	ctx.fillStyle = DEAD_COLOR;
+	for (let row = 0; row < height; row++) {
+		for (let col = 0; col < height; col++) {
+			const idx = getIndex(row, col);
+			if (cells[idx] !== Cell.Dead) {
+				continue;
+			}
+
+			ctx.fillRect(
+				col * (CELL_SIZE + 1) + 1,
+				row * (CELL_SIZE + 1) + 1,
+				CELL_SIZE,
+				CELL_SIZE,
 			);
 		}
 	}
